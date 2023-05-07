db0144820f5de16c2729dc334f8f66ecc7dcac66
code kazuki <agbowurotoyin8@gmail.com>
2023-05-07 22:49:54 +0100

    message

--- assignment.js
+++ assignment.js
@@ -1,18 +1,1 @@
 "use strict";
-
-var x = 35;
-if (x === 5) {
-  console.log("var");
-} else {
-  console.log("ttttttt");
-}
-
-const hover = document.querySelector(".items");
-
-hover.addEventListener("click", function () {
-  if (style.color == red) {
-    style.color = "green";
-  } else {
-    style.color = "yellow";
-  }
-});
