6d392611eb4c3c57a338adf4fa9d7be0c226250a
code kazuki <agbowurotoyin8@gmail.com>
2023-05-08 23:22:17 +0100

    message

--- assignment.js
+++ assignment.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// const hideButton = function (x) {
+//   x.stle.dispaly = "none";
+// };
+function hideButton(x) {
+  x.style.display = "none";
+}
+
+function changeColor(x) {
+  x.style.color = "red";
+}
