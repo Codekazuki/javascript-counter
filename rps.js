f99282f499954e260bfb967616d7313bd67c83d1
code kazuki <agbowurotoyin8@gmail.com>
2023-06-20 11:01:40 +0100

    latest changes

--- rps.js
+++ rps.js
@@ -1,10 +1,14 @@
-console.log("hello rps");
-let randomNumber = Math.random();
+let computerMove = "";
 
-if (randomNumber >= 0 && randomNumber <= 1 / 3) {
-  console.log("rock");
-} else if (randomNumber >= 1 / 3 && randomNumber <= 2 / 3) {
-  console.log("paper");
-} else {
-  console.log("scissors");
+function pickComputerMove() {
+  let randomNumber = Math.random();
+  let computerMove = "";
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "Rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "Paper";
+  } else {
+    computerMove = "scissors";
+  }
 }
