1834a82e819d63f6726431d756e9233f20ca41a2
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-11 11:41:03 +0100

    javascript counter

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+    addColor();
+  }
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+  }
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+  }
+});
+
+function addColor() {
+  if (count.innerHTML > 0) {
+    count.style.color = "red";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "yellow";
+  } else {
+    count.style.color = "green";
+  }
+}
